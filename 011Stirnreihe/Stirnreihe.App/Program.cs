cf9fe68b02f0c300c52b15de79f329da59e3c493
Simon Koller <113828769+shotgun007@users.noreply.github.com>
2023-12-08 21:44:05 +0100

    Add files via upload

---
+++ 011Stirnreihe/Stirnreihe.App/Program.cs
@@ -0,0 +1,99 @@
+﻿using Stirnreihe.Logic;
+string selection;
+LineOfPeople l = new();
+string lastName = "";
+string firstName = "";
+int height = 0;
+
+Console.WriteLine("Welcome to the Stirnreihe World Record App! What do you want to do?");
+Console.WriteLine("1) Add a person to the line");
+Console.WriteLine("1b) Add a person to the line (sorted)");
+Console.WriteLine("2) Print the line");
+Console.WriteLine("3) Clear the line");
+Console.WriteLine("4) Exit");
+do
+{
+    selection = Console.ReadLine()!;
+    Console.WriteLine($"Your choice {selection}");
+
+    switch (selection)
+    {
+        case "1":
+            // Daten für eine neue Person werden abgefragt
+            Console.Write("Last Name: ");
+            lastName = Console.ReadLine()!;
+            Console.Write("First Name: ");
+            firstName = Console.ReadLine()!;
+            Console.Write("Height (cm): ");
+            height = int.Parse(Console.ReadLine()!);
+            // Person wird erstellt
+            var newPerson = new Person(lastName, firstName, height);
+            // Person wird vorne hinzugefügt
+            l.AddToFront(newPerson);
+            break;
+        case "1b":
+             // Daten für eine neue Person werden abgefragt
+            Console.Write("Last Name: ");
+            lastName = Console.ReadLine()!;
+            Console.Write("First Name: ");
+            firstName = Console.ReadLine()!;
+            Console.Write("Height (cm): ");
+            height = int.Parse(Console.ReadLine()!);
+            // Person wird erstellt
+            var newPerson2 = new Person(lastName, firstName, height);
+            // Person wird sortiert nach Größe hinzugefügt
+            l.AddSorted(newPerson2);
+            break;
+        case "2":
+            // wird ausgegeben wenn keine Personen vorhanden sind
+            if (l.First is null)
+            {
+                Console.WriteLine("Empty line");
+            }
+            // Alle vorhandenen Personen werden ausgegeben
+            else
+            {
+                var ElementToDisplay = l.First;
+                Console.WriteLine(ElementToDisplay.Person.ToString());
+                while (ElementToDisplay?.Next is not null)
+                {
+                    ElementToDisplay = ElementToDisplay.Next;
+                    Console.WriteLine(ElementToDisplay.Person.ToString());
+                }
+            }
+            break;
+        case "3":
+            // Alle Personen werden gelöscht
+            l.Clear();
+            Console.WriteLine("Cleared");
+            break;
+        case "4":
+            // Programm wird beendet
+            Console.WriteLine("Exit");
+            break;
+        default:
+            Console.WriteLine("Invalid input");
+            break;
+    }
+}
+while (selection != "4");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 011Stirnreihe/Stirnreihe.Logic/Class1.cs
@@ -0,0 +1,94 @@
+﻿namespace Stirnreihe.Logic;
+
+public class Person (string lastName, string firstName, int height)
+{
+    public string LastName { get; set; } = lastName;
+    public string FirstName { get; set; } = firstName;
+    public int Height { get; set; } = height;
+    // Die Methode ToString() gibt Nachname, Vorname und Größe einer Person aus
+    public override string ToString()
+    {
+        return $"{LastName}, {FirstName} ({Height} cm)";
+    }
+}
+
+// Mit dieser Klasse werden verknüpfungen erstellt
+public class Node(Person person, Node? next)
+{
+    public Person Person { get; } = person;
+    public Node? Next { get; set; } = next;
+}
+
+public class LineOfPeople
+{
+    public Node? First { get; set; } = null;
+    // Die neue Person wird vorne hinzugefügt
+    public void AddToFront(Person person)
+    {
+        // neue Person wird erstellt und First wird newPerson.Next
+        var newPerson = new Node(person, First);
+        First = newPerson;
+    }
+    public int AddSorted(Person person)
+    {
+        var newPerson = new Node(person, null);
+        if (First is null || newPerson.Person.Height < First.Person.Height)
+        {
+            AddToFront(person);
+            return 0;
+        }
+        else
+        {
+            int index;
+            var help = First;
+            // Der richtige Platz für die Person wird gesucht
+            for (index = 1; help.Next != null && help.Next.Person.Height <=  newPerson.Person.Height; index++)
+            {
+                help = help.Next;
+            }
+            // die neue Person wird eingefügt
+            newPerson.Next = help.Next;
+            help.Next = newPerson;
+            return index;
+        }
+    }
+    public void Clear()
+    {
+        // First wird gelöscht, der Rest wird vom Garbage collector bereinigt
+        First = null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
